4054b2d1f405bffd327043c1afb940eb2532b28f
Leonardo2M <leonardomm.contato@gmail.com>
2023-06-15 23:26:39 -0300

    feat: criado VO DadosPessoal

---
+++ cadastro/src/domain/model/CPF.java
@@ -0,0 +1,22 @@
+package domain.model;
+
+public class CPF {
+
+    private String numero;
+
+    public CPF(String numero) {
+        if(numero == null || !numero.matches("\\d{3}\\.\\d{3}\\.\\d{3}\\-\\d{2}")) {
+            throw new IllegalArgumentException("CPF inválido");
+        }
+
+        this.numero = numero;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+}
---
+++ cadastro/src/domain/model/DadosPessoal.java
@@ -0,0 +1,12 @@
+package domain.model;
+
+import java.time.LocalDate;
+
+public class DadosPessoal {
+
+    private String nome;
+    private CPF cpf;
+    private LocalDate dataNascimento;
+    private Sexo sexo;
+
+}
