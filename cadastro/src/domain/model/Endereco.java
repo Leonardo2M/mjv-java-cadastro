002acdf29b68b2fa9ffeee62d9154321be03f98b
Leonardo2M <leonardomm.contato@gmail.com>
2023-06-15 23:30:16 -0300

    feat: criado VO para dados do Endereco

---
+++ cadastro/src/domain/model/Endereco.java
@@ -0,0 +1,12 @@
+package domain.model;
+
+public class Endereco {
+
+    private String logradouro;
+    private String numero;
+    private String complemento;
+    private String bairro;
+    private String cidade;
+    private String estado;
+
+}
