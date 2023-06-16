76be8d532ab59e56234be4a66d9666f395abfff6
Leonardo2M <leonardomm.contato@gmail.com>
2023-06-16 00:04:22 -0300

    feat: novo VO para dados profissional

---
+++ cadastro/src/domain/model/profissional/DadosProfisional.java
@@ -0,0 +1,13 @@
+package domain.model.profissional;
+
+import java.math.BigDecimal;
+
+public class DadosProfisional {
+
+    private String profissao;
+    private String Empresa;
+    private BigDecimal salario;
+    private Boolean empregado;
+    private PretensaoSalarial pretensaoSalarial;
+
+}
---
+++ cadastro/src/domain/model/profissional/PretensaoSalarial.java
@@ -0,0 +1,10 @@
+package domain.model.profissional;
+
+import java.math.BigDecimal;
+
+public class PretensaoSalarial {
+
+    private BigDecimal valorMinimo;
+    private BigDecimal valorMaximo;
+
+}
